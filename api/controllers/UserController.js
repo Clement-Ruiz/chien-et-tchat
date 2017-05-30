765738280fe44a07c166d874bcbc9630e55a615f
Clement-Ruiz <clement.ruiz@ynov.Com>
2017-05-30 22:44:33 +0200

    Removing Signup action + handling homepage

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -7,11 +7,19 @@ _.merge(exports, _super);
 _.merge(exports, {
 
   // Extend with custom logic here by adding additional fields, methods, etc.
-
+  homepage: function(req, res){
+    var auth=req.session.authenticated || false;
+    var username='';
+    if(req.user){ username = req.user.username }
+    return res.view("homepage", {
+      auth: auth,
+      user: username
+    });
+  },
   // Return the view of user's profile.
   profile: function(req, res){
-    var id = req.param('id') || req.user.id;
-    User.findOne({ id: id }).exec(function(err, user){
+    var name = req.param('name') || req.user.username;
+    User.findOne({ username: name }).exec(function(err, user){
       if(err){
         sails.log.error('Error while finding User');
         sails.log.error(err);
@@ -27,43 +35,5 @@ _.merge(exports, {
         });
       }
     });
-  },
-
-  // Signup function
-  signup: function(req, res){
-    var username = req.param('username');
-    if (!username){
-      return res.send(500, "Username is required");
-    }
-    var email = req.param('email');
-    var password = req.param('password');
-    var confirm = req.param('confirm');
-    var firstName = req.param('firstName') || 'Huille';
-    var lastName = req.param('lastName') || 'Smisse';
-    if (password != confirm){
-      return res.send(500, "Passwords don't match");
-    }
-    User.create({
-      username: username,
-      email: email,
-      password: password,
-      firstName: firstName,
-      lastName: lastName
-    }).exec(function(err, user){
-      if(err){
-        sails.log.error(err);
-        return res.negotiate(err);
-      }
-      if(!user){
-        sails.log.error('Signup - No user returned after User.create()');
-        return res.send(500, 'User creation failed');
-      }
-      else{
-        sails.log.info('Signup - New user created !');
-        sails.log.info(user);
-        return res.ok(user);
-      }
-    });
-
   },
 });
